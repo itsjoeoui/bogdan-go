b5efda34a4a030b9c79fa0b02bfe5c34f3d5ad57
Joey Yu <joey@itsjoeoui.com>
2021-08-05 23:19:58 -0400

    Add some comments
    Signed-off-by: Joey Yu <joey@itsjoeoui.com>

--- main.go
+++ main.go
@@ -11,36 +11,41 @@ import (
 )
 
 func main() {
+	// Load the token using godotenv.
 	err := godotenv.Load(".env")
 	if err != nil {
 		fmt.Println(err)
 	}
-	fmt.Println(os.Getenv("DISCORD"))
 
+	// Create a dg session.
 	dg, err := discordgo.New("Bot " + os.Getenv("DISCORD"))
-
 	if err != nil {
 		fmt.Println(err)
 	}
 
 	dg.AddHandler(messageCreate)
 
+	// Open a connection to Discord.
 	err = dg.Open()
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
 
+
 	fmt.Println("Bot is now running. Press CTRL-C to exit.")
 
+	// Listen for the CTRL-C command.
 	c := make(chan os.Signal, 1)
 	signal.Notify(c, os.Interrupt)
 	<-c
 
+	// Close down everything.
 	dg.Close()
 }
 
 func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
+	// Ignore everythong from the bot itself.
 	if m.Author.ID == s.State.User.ID {
 		return
 	}
