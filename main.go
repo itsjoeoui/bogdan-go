45438736f06af52f8e21e9847b1714d132153c9a
Joey Yu <joey@itsjoeoui.com>
2021-08-05 23:13:34 -0400

    The hello world equivalent
    Signed-off-by: Joey Yu <joey@itsjoeoui.com>

--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"os"
+	"os/signal"
+	"strings"
 
 	"github.com/bwmarrin/discordgo"
 	"github.com/joho/godotenv"
@@ -20,5 +22,30 @@ func main() {
 	if err != nil {
 		fmt.Println(err)
 	}
+
+	dg.AddHandler(messageCreate)
+
+	err = dg.Open()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	fmt.Println("Bot is now running. Press CTRL-C to exit.")
+
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt)
+	<-c
+
 	dg.Close()
+}
+
+func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
+	if m.Author.ID == s.State.User.ID {
+		return
+	}
+
+	if strings.ToLower(m.Content) == "bruh" {
+		s.ChannelMessageSend(m.ChannelID, "bruh")
+	}
 }
